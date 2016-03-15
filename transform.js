4077d76d3f3ba65e4ac8c4d9a6b2ae7b51eb7594
Sid <sid.ghodke@gmail.com>
2016-03-14 22:26:57 -0700

    Updating Usage notes

--- transform.js
+++ transform.js
@@ -3,9 +3,16 @@
  *
  * Example:
  *
- *  node transform.js log-2016-03-13-09:00:19.doctor_web.log | 
+ *  node transform.js web-2016-03-13-09:00:19.log | 
  *    bunyan -c 'this.module == "router"'
  * 
+ *  ssh -c "cat log.file" | node transform.js | bunyan
+ *
+ * Optional flags:
+ *  --pid=-1           Overwrite the system pid, during transformation
+ *  --hostname=Beaker  Overwrite the hostname, during transformation
+ *  --name="WebApp"    Set the application name
+ *
  */
 
 var _ = require('underscore');
@@ -16,20 +23,20 @@ var bunyan = require('bunyan');
 var log = bunyan.createLogger({name: argv.name || 'Doctor WebApp'});
 
 
-function _bunyanize(l) {
+function _bunyanize(item) {
   var metadata = ['module', 'event'];
-  var formattedTime = new Date(l.timestamp).toUTCString()
+  var formattedTime = new Date(item.timestamp).toUTCString()
 
-  log[l.level.toLowerCase() || 'info'](
-      // Meta Data
-      _.extend(
-        { time: formattedTime }, 
-        _.pick(l.data, metadata),
-        _.omit(argv, '_', 'name')
-      ), 
+  log[item.level.toLowerCase() || 'info'](
+    // Meta Data
+    _.extend(
+      { time: formattedTime }, 
+      _.pick(item.data, metadata),
+      _.omit(argv, '_', 'name')
+    ), 
 
-      // Payload
-      _.omit(l.data, metadata, 'timestamp')
+    // Payload
+    _.omit(item.data, metadata, 'timestamp')
   );
 }
 
@@ -40,8 +47,8 @@ function bunyanize(fileName) {
 }
 
 function fromStdin() {
-  var stdin = process.stdin,
-      inputChunks = [];
+  var stdin = process.stdin;
+  var inputChunks = [];
 
   stdin.resume();
   stdin.setEncoding('utf8');
@@ -51,8 +58,8 @@ function fromStdin() {
   });
 
   stdin.on('end', function () {
-    var inputJSON = inputChunks.join(),
-        parsedData = JSON.parse(inputJSON);
+    var inputJSON = inputChunks.join();
+    var parsedData = JSON.parse(inputJSON);
 
     parsedData.map(_bunyanize);
   });
